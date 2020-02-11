bd3be6ee111c4789169362867d5a28cf91ab2972
CarlosAlvaIqui <carlos.alvarez.i@tecsup.edu.pe>
2020-02-11 17:53:11 -0500

    Mejorando detalles por un futuro mejor

--- lobitofh/src/components/Reactivescom/Detailswork.js
+++ lobitofh/src/components/Reactivescom/Detailswork.js
@@ -8,6 +8,13 @@ import Container from '@material-ui/core/Container';
 import perfilphoto from '../../assets/img/hppisfree.jpg';
 import { Link, NavLink } from 'react-router-dom';
 
+
+import Rating from '@material-ui/lab/Rating';
+import StarBorderIcon from '@material-ui/icons/StarBorder';
+import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
+
+
 const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
@@ -34,154 +41,185 @@ const Detailswork = (props) => {
       </div>
       <div>
         <CssBaseline />
-        <Container maxWidth="lg">
+        <Container maxWidth="md">
           <Typography component="div" style={{}}>
             <Grid container spacing={3}>
 
               <Grid item xs={12} sm={7}>
+                <p>
+                  <strong className="titlebig">
+                    Detalle del trabajo
+                    </strong>
+                </p>
+                <br />
+
+                <div>
                   <p>
-                    <strong className="titlebig">
-                      Detalle del trabajo
-                    </strong>
+                    <strong className="titlespace">
+                      Lugar del trabajo a realizar
+            </strong>
+                    <br />
+                    <span className="mispa">
+                      Establece la direccion exacta
+            </span>
                   </p>
-                  <br />
-
-                  <div>
-                    <p>
-                      <strong className="titlespace">
-                        Lugar del trabajo a realizar
-            </strong>
+                  <div className="divradi">
+                    <div className="tothel">
+                      <span>calle</span>
                       <br />
-                      <span className="mispa">
-                        Establece la direccion exacta
-            </span>
-                    </p>
-                    <div className="divradi">
-                      <div className="tothel">
-                        <span>calle</span>
-                        <br />
-                        <span>Av. Ejercito</span>
-                      </div>
-                      <div className="tothell">
-                        <span>Datos(piso puerta)</span>
-                        <br />
-                        <span>Sin datos</span>
-                      </div>
-                      <div className="tother">
-                        <span></span>
-                        <br />
-                        <span className="movet">Editar</span>
-                      </div>
-
+                      <span>Av. Ejercito</span>
                     </div>
-                  </div>
-
-                  <br />
-                  <br />
-
-
-                  <div>
-                    <p>
-                      <strong className="titlespace">
-                        Hora
-            </strong>
+                    <div className="tothell">
+                      <span>Datos(piso puerta)</span>
                       <br />
-
-                      <span className="mispa">
-                        Hora programada
-            </span>
-                    </p>
-                    <div className="divradi">
-
-                      <div className="halfdiv">
-                        <span>Fecha y hora</span>
-                        <br />
-                        <span>Vierner 31 de Enero del 2020</span>
-                      </div>
-                      <div className="halfdivc">
-                        <span></span>
-                        <br />
-                        <span className="movet">15:00</span>
-                      </div>
-
+                      <span>Sin datos</span>
                     </div>
+                    <div className="tother">
+                      <span></span>
+                      <br />
+                      <span className="movet">Editar</span>
+                    </div>
+
                   </div>
+                </div>
 
-                  <br />
-                  <br />
+                <br />
+                <br />
+
+
+                <div>
+                  <p>
+                    <strong className="titlespace">
+                      Hora
+            </strong>
+                    <br />
+
+                    <span className="mispa">
+                      Hora programada
+            </span>
+                  </p>
+                  <div className="divradi">
+
+                    <div className="halfdiv">
+                      <span>Fecha y hora</span>
+                      <br />
+                      <span>Vierner 31 de Enero del 2020</span>
+                    </div>
+                    <div className="halfdivc">
+                      <span></span>
+                      <br />
+                      <span className="movet">15:00</span>
+                    </div>
+
+                  </div>
+                </div>
+
+                <br />
+                <br />
 
 
 
-                  <div>
-                    <p>
-                      <strong className="titlespace">
-                        Datos del tecnico
+                <div>
+                  <p>
+                    <strong className="titlespace">
+                      Datos del tecnico
                         </strong>
-                      <br />
+                    <br />
 
-                      <span className="mispa">
-                        Revisa los datos del tecnico
+                    <span className="mispa">
+                      Revisa los datos del tecnico
                         </span>
-                    </p>
-                    <div className="cartatecnico">
-                      <div className="headercard">
-                          <p>TecniGO</p>
+                  </p>
+                  <div className="cartatecnico">
+                    <div className="headercard">
+                      <p>TecniGO</p>
+                    </div>
+                    <div className="tecnicodata">
+                      <div className="divimgtec">
+                        <img src={perfilphoto} style={{ width: 150 }} className="tecper" />
+
                       </div>
-                      <div className="perfildata">
+
+                      <div className="divdatatec">
+                        <strong>
+                          <span className="infotec">TEC. Diana Tumba Torres </span>
+                        </strong>
+                        <div className="datat">
+                          <p className="mite">Tecnico en Computacion</p>
+                          <p className="mite">28 Años</p>
+                          <p className="mite">Dtuma.torres@gmail.com</p>
+                          <p className="mite">963258753 - 963254178</p>
                           <div>
-                              <img src={perfilphoto} style={{width:200}} className="tecper"/>
+                            <Box component="fieldset" mb={3} borderColor="transparent">
+                              <Rating
+                                name="customized-empty"
+                                defaultValue={2}
+                                precision={0.5}
+                                emptyIcon={<StarBorderIcon fontSize="inherit" />}
+                              />
+                              <Button variant="contained" color="secondary">
+                                Calificar
+                                          </Button>
+
+                            </Box>
+
                           </div>
-                          <div className="divdatatec">
-                              <strong>
-                              <span className="infotec">TEC. Diana Tumba Torres </span>
-                              </strong>
-                          </div>
-                      </div>
-                      <div className="centerthis"> 
-                          <hr />
-                              <span >El hombre se esta poniendo la soga al cuello</span>
-                          <hr />
+                        </div>
                       </div>
                     </div>
+                    <div className="centerthis">
+                      <hr />
+                      <p >El hombre se esta poniendo la soga al cuello</p>
+                      <hr />
+                    </div>
+
+
+                    {
+                      /**
+                       *  
+                       */
+                    }
+
                   </div>
+                </div>
 
-                  <br />
-                  <br />
+                <br />
+                <br />
 
 
-                  <div>
-                    <p>
-                      <strong className="titlespace">
-                        Pago
+                <div>
+                  <p>
+                    <strong className="titlespace">
+                      Pago
                      </strong>
-                      <br />
+                    <br />
 
-                      <span className="mispa">
-                        Realizar el pago
+                    <span className="mispa">
+                      Realizar el pago
               </span>
-                    </p>
-                  </div>
+                  </p>
+                </div>
 
-                  <br />
-                  <br />
+                <br />
+                <br />
 
-                  <div>
-                    <p>
-                      <strong className="titlespace">
-                        Informe
+                <div>
+                  <p>
+                    <strong className="titlespace">
+                      Informe
                     </strong>
-                      <br />
+                    <br />
 
-                      <span className="mispa">
-                        Lea el informe
+                    <span className="mispa">
+                      Lea el informe
                       </span>
-                        <br/>
-                      <Link className="nav-bar-brand" >
-                        <span>Ver Informe</span>
-                        </Link>
-                      
-                    </p>
-                  </div>
+                    <br />
+                    <Link className="nav-bar-brand" to={"/"}>
+                      <span>Ver Informe</span>
+                    </Link>
+
+                  </p>
+                </div>
               </Grid>
               <Grid item xs={12} sm={5}>
               </Grid>
